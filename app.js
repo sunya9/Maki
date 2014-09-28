1aab01acbd0169eb2fe40353b7f146bda844be4a
sunya <mugen.Xyz@gmail.com>
2014-09-28 20:14:40 +0900

    delte some files.

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ var socketIO = require('socket.io');
 var multer = require('multer');
 var settings = require('./lib/init');
 var apiRouter = require('./lib/api_router');
+// var dbWrapper = require('./lib/api/db_wrapper');
 
-// api endpoint
-// var upload = require('./routes/upload');
 
 // create instance
 var app = express();
@@ -31,6 +30,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ect');
 
 
+// User init
+// set routing
+
+var pref = new settings('settings.json');
+
+apiRouter(app);
+// dbWrapper.init(pref.prefs.db_name);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -38,17 +45,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 app.use(multer({
-    dest: './musics'
-}))
+    dest: pref.prefs.music_folder 
+    // dest : '/musics'
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// set routing
-// app.use('/upload', upload.);
-apiRouter(app);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,8 +60,6 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
-var pref = new settings('settings.json');
 
 // error handlers
 
